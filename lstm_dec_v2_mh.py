divide multihead hidden size by num of heads, fix average batch loss, add commandline arg for result path

lstm_dec_v2_mh.py
@@ -16,9 +16,9 @@ class LSTMAttentionDecoder(nn.Module):
         self.is_predicting = is_predicting
         self.device = device
         self.copy = copy
-        self.coverage = coverage 
+        self.coverage = coverage
         self.n_heads = n_heads
-        
+
         self.lstm_1 = nn.LSTMCell(self.input_size, self.hidden_size)
 
         self.Wx = nn.Parameter(torch.Tensor(4 * self.hidden_size, self.ctx_size))
@@ -30,17 +30,24 @@ class LSTMAttentionDecoder(nn.Module):
         self.W_comb_att = nn.ParameterList()
         self.U_att = nn.ParameterList()
         self.W_coverage = nn.ParameterList()
-        self.W_att_concat = nn.Linear(self.ctx_size * self.n_heads, self.ctx_size, bias=False)
+        self.Wv_att = nn.ParameterList()
+        self.bv_att = nn.ParameterList()
 
+        self.W_att_concat = nn.Linear(self.ctx_size, self.ctx_size, bias=False)
+        if self.ctx_size % self.n_heads != 0:
+            raise RuntimeError("ctx_size should be a multiple of n_heads.")
         for i in range(self.n_heads):
-            self.Wc_att.append(nn.Parameter(torch.Tensor(self.ctx_size, self.ctx_size)))
-            self.b_att.append(nn.Parameter(torch.Tensor(self.ctx_size)))
+            self.Wc_att.append(nn.Parameter(torch.Tensor(self.ctx_size // self.n_heads, self.ctx_size)))
+            self.b_att.append(nn.Parameter(torch.Tensor(self.ctx_size // self.n_heads)))
 
-            self.W_comb_att.append(nn.Parameter(torch.Tensor(self.ctx_size, 2*self.hidden_size)))
-            self.U_att.append(nn.Parameter(torch.Tensor(1, self.ctx_size)))
+            self.Wv_att.append(nn.Parameter(torch.Tensor(self.ctx_size // self.n_heads, self.ctx_size)))
+            self.bv_att.append(nn.Parameter(torch.Tensor(self.ctx_size // self.n_heads)))
+
+            self.W_comb_att.append(nn.Parameter(torch.Tensor(self.ctx_size // self.n_heads, 2*self.hidden_size)))
+            self.U_att.append(nn.Parameter(torch.Tensor(1, self.ctx_size // self.n_heads)))
 
             if self.coverage:
-                self.W_coverage.append(nn.Parameter(torch.Tensor(self.ctx_size, 1)))
+                self.W_coverage.append(nn.Parameter(torch.Tensor(self.ctx_size // self.n_heads, 1)))
 
         self.init_weights()
 
@@ -75,17 +82,17 @@ class LSTMAttentionDecoder(nn.Module):
         def recurrence(x, y_mask, hidden, pctx, context, x_mask, acc_att=None):
             pre_h, pre_c = hidden
 
-            h1, c1 = self.lstm_1(x, hidden)  
+            h1, c1 = self.lstm_1(x, hidden)
             h1 = y_mask * h1 + (1.0 - y_mask) * pre_h
             c1 = y_mask * c1 + (1.0 - y_mask) * pre_c
-            
+
             # len(x) * batch_size * 1
             s = T.cat((h1.view(-1, self.hidden_size), c1.view(-1, self.hidden_size)), 1)
             if self.coverage:
                 word_atten = [_get_word_atten(pctx, s, x_mask, i, acc_att) for i in range(self.n_heads)]
             else:
                 word_atten = [_get_word_atten(pctx, s, x_mask, i) for i in range(self.n_heads)]
-            atted_ctx_heads = [T.sum(word_atten[i] * context, 0) for i in range(self.n_heads)]
+            atted_ctx_heads = [T.sum(word_atten[i] * context[i], 0) for i in range(self.n_heads)]
             atted_ctx_concat = T.cat(atted_ctx_heads, -1)
             atted_ctx = self.W_att_concat(atted_ctx_concat)
 
@@ -105,7 +112,7 @@ class LSTMAttentionDecoder(nn.Module):
 
             word_atten_avg = torch.mean(torch.stack(word_atten, 0), dim=0)
             word_atten_avg = T.transpose(word_atten_avg.view(x_mask.size(0), -1), 0, 1)
-            
+
             if self.coverage:
                 acc_att += word_atten_avg
                 return (h2, c2), h2, atted_ctx, word_atten_point, acc_att
@@ -115,32 +122,33 @@ class LSTMAttentionDecoder(nn.Module):
         hs, cs, ss, atts, dists, xids, Cs = [], [], [], [], [], [], []
         hidden = init_state
         acc_att = init_coverage
-        if self.copy: 
+        if self.copy:
             xid = T.transpose(xid, 0, 1) # B * len(x)
 
         pctx = [F.linear(context, self.Wc_att[i], self.b_att[i]) for i in range(self.n_heads)]
+        pv = [F.linear(context, self.Wv_att[i], self.bv_att[i]) for i in range(self.n_heads)]
         x = y_emb
-        
+
         steps = range(y_emb.size(0))
         for i in steps:
             if self.coverage:
                 Cs += [acc_att]
-                hidden, s, att, att_dist, acc_att = recurrence(x[i], y_mask[i], hidden, pctx, context, x_mask, acc_att)
+                hidden, s, att, att_dist, acc_att = recurrence(x[i], y_mask[i], hidden, pctx, pv, x_mask, acc_att)
             else:
-                hidden, s, att, att_dist = recurrence(x[i], y_mask[i], hidden, pctx,context, x_mask)
+                hidden, s, att, att_dist = recurrence(x[i], y_mask[i], hidden, pctx, pv, x_mask)
             hs += [hidden[0]]
             cs += [hidden[1]]
             ss += [s]
             atts += [att]
             dists += [att_dist]
             xids += [xid]
-        
+
         if self.coverage:
             if self.is_predicting :
                 Cs += [acc_att]
                 Cs = Cs[1:]
             Cs = T.stack(Cs).view(y_emb.size(0), *Cs[0].size())
-        
+
 
         hs = T.stack(hs).view(y_emb.size(0), *hs[0].size())
         cs = T.stack(cs).view(y_emb.size(0), *cs[0].size())
@@ -149,7 +157,7 @@ class LSTMAttentionDecoder(nn.Module):
         dists = T.stack(dists).view(y_emb.size(0), *dists[0].size())
         if self.copy:
             xids = T.stack(xids).view(y_emb.size(0), *xids[0].size())
-        
+
         if self.copy and self.coverage:
             return (hs, cs), ss, atts, dists, xids, Cs
         elif self.copy:

utils_pg.py
@@ -65,7 +65,7 @@ def save_model(f, model, optimizer):
     torch.save({"model_state_dict" : model.state_dict(),
             "optimizer_state_dict" : optimizer.state_dict()},
             f)
- 
+
 def load_model(f, model, optimizer):
     checkpoint = torch.load(f)
     model.load_state_dict(checkpoint["model_state_dict"])
@@ -75,7 +75,7 @@ def load_model(f, model, optimizer):
 def sort_samples(x, len_x, mask_x, y, len_y, \
                  mask_y, oys, x_ext, y_ext, oovs):
     sorted_x_idx = np.argsort(len_x)[::-1]
-    
+
     sorted_x_len = np.array(len_x)[sorted_x_idx]
     sorted_x = x[:, sorted_x_idx]
     sorted_x_mask = mask_x[:, sorted_x_idx, :]
@@ -87,7 +87,7 @@ def sort_samples(x, len_x, mask_x, y, len_y, \
     sorted_oys = [oys[i] for i in sorted_x_idx]
     sorted_x_ext = x_ext[:, sorted_x_idx]
     sorted_y_ext = y_ext[:, sorted_x_idx]
-    
+
     return sorted_x, sorted_x_len, sorted_x_mask, sorted_y, \
            sorted_y_len, sorted_y_mask, sorted_oys, \
            sorted_x_ext, sorted_y_ext, sorted_oovs
@@ -103,7 +103,7 @@ def print_sent_dec(y_pred, y, y_mask, oovs, modules, consts, options, batch_size
         print idx_doc + 1, "----------------------------------------------------------------------------------------------------"
         sent_true= ""
         for idx_word in range(max_y_words[idx_doc]):
-            i = y[idx_word, idx_doc] if options["has_learnable_w2v"] else np.argmax(y[idx_word, idx_doc]) 
+            i = y[idx_word, idx_doc] if options["has_learnable_w2v"] else np.argmax(y[idx_word, idx_doc])
             if i in modules["i2w"]:
                 sent_true += modules["i2w"][i]
             else:
@@ -131,7 +131,7 @@ def print_sent_dec(y_pred, y, y_mask, oovs, modules, consts, options, batch_size
         else:
             print sent_pred
     print "----------------------------------------------------------------------------------------------------"
-    print 
+    print
 
 
 def write_for_rouge(fname, ref_sents, dec_words, cfg):
@@ -218,5 +218,3 @@ def write_summ(dst_path, summ_list, num_summ, options, i2w = None, oovs=None, sc
 
                 f_summ.write(s)
                 f_summ.write("\n")
-
-