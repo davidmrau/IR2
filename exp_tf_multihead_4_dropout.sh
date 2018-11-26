8068b3bd68c88c108d4ec9bc07675d860310c6c5
David Rau <david.rau@hotmail.de>
2018-11-26 15:52:03 +0100

    add mkdir, add tf schedule offset

--- exp_tf_multihead_4_dropout.sh
+++ exp_tf_multihead_4_dropout.sh
@@ -1,2 +1,3 @@
 BASE=results/exp_tf_multihead_4_dropout
-python2 main.py --tf_schedule --n_heads 4 --dropout_p_point --result_path $BASE >> $BASE/log.txt
+mkdir -p $BASE
+python2 main.py --tf_schedule --tf_offset 200000 --n_heads 4 --dropout_p_point --result_path $BASE >> $BASE/log.txt
--- exp_tf_multihead_4_w_prior_point_loss.sh
+++ exp_tf_multihead_4_w_prior_point_loss.sh
@@ -1,2 +1,3 @@
 BASE=results/exp_tf_multihead_4_w_prior_point_loss
-python2 main.py --tf_schedule --n_heads 4 --use_w_prior_point_loss --result_path $BASE >> $BASE/log.txt
+mkdir -p $BASE
+python2 main.py --tf_schedule --tf_offset 200000 --n_heads 4 --use_w_prior_point_loss --result_path $BASE >> $BASE/log.txt
