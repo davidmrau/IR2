d6b37772e72bc880c9dfc359cdf58fe34964ab71
David Rau <david.rau@hotmail.de>
2018-11-26 15:12:39 +0100

    add experiment scripts

---
+++ experiments/exp_tf_multihead_4.sh
@@ -0,0 +1,1 @@
+python ../main.py --tf_schedule --n_heads 4 --result_path results/exp_tf_multihead_4
---
+++ experiments/exp_tf_multihead_4_dropout.sh
@@ -0,0 +1,1 @@
+python ../main.py --tf_schedule --n_heads 4 --dropout_p_point --result_path results/exp_tf_multihead_4_dropout
