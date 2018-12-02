53ebb47d243eda5285faf0a81d278692d92a99e4
EvdWel <eelcovdw@gmail.com>
2018-12-02 18:42:23 +0100

    train script for mh

---
+++ exp_mh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=multihead
+#SBATCH --ntasks=1
+#SBATCH --time=4:00:00
+#SBATCH --partition=gpu_shared
+#SBATCH --gres=gpu:1
+
+module load cuDNN/7.0.5-CUDA-9.0.176
+module purge
+module load NCCL/2.0.5-CUDA-9.0.176
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-G$
+
+BASE=~/IR2/results/exp_mh
+mkdir -p $BASE
+python2 ~/IR2/main.py --batch_size 16 --n_heads 4 --result_path $BASE
