b45a30f1de34c96448a0eed4b6ad04a9c801431f
davidmrau <david.rau@hotmail.de>
2018-12-12 14:43:03 +0100

    set cov to true, write eval into folder that is named after model

--- exp_eval.sh
+++ exp_eval.sh
@@ -24,27 +24,28 @@ OUTPUTDIR=~/IR2/$1/
 #MODELFOLDER=~/IR2/$BASE/$4/
 
 
-mkdir -p $OUTPUTDIR
+mkdir -p $OUTPUTDIR/$MODELNAME
 
 echo $OUTPUTDIR
 
-python2 ~/IR2/main.py --n_heads $3 --result_path $OUTPUTDIR --predict --model_name $MODELNAME --output_dir $OUTPUTDIR
-python2 ~/IR2/prepare_rouge.py --result_path $OUTPUTDIR
+python2 ~/IR2/main.py --n_heads $3 --result_path $OUTPUTDIR/$MODELNAME --predict --model_name $OUTPUTDIR/model/$MODELNAME --output_dir $OUTPUTDIR/$MODELNAME
+python2 ~/IR2/prepare_rouge.py --result_path $OUTPUTDIR/$MODELNAME
 
 
-python2 ~/IR2/ngram_overlap.py $OUTPUTDIR ~/deepmind/test_set/test.pkl > $OUTPUTDIR/ngramoverlap
-cat $OUTPUTDIR/ngramoverlap
-rm $OUTPUTDIR/generated.txt
-rm $OUTPUTDIR/targets.txt
-ls $OUTPUTDIR/summary/ | sort -n | while read L; do 
-	cat $OUTPUTDIR/summary/$L | tr -d '\n\r' >> $OUTPUTDIR/generated.txt
-	echo >> $OUTPUTDIR/generated.txt
+python2 ~/IR2/ngram_overlap.py $OUTPUTDIR/$MODELNAME ~/deepmind/test_set/test.pkl > $OUTPUTDIR/$MODELNAME/ngramoverlap
+#python2 ~/IR2/ngram_overlap.py $OUTPUTDIR ~/deepmind/test_set/test_1100.pkl > $OUTPUTDIR/ngramoverlap
+cat $OUTPUTDIR/$MODELNAME/ngramoverlap
+rm $OUTPUTDIR/$MODELNAME/generated.txt
+rm $OUTPUTDIR/$MODELNAME/targets.txt
+ls $OUTPUTDIR/$MODELNAME/summary/ | sort -n | while read L; do 
+	cat $OUTPUTDIR/$MODELNAME/summary/$L | tr -d '\n\r' >> $OUTPUTDIR/$MODELNAME/generated.txt
+	echo >> $OUTPUTDIR/$MODELNAME/generated.txt
 done
-ls $OUTPUTDIR/beam_ground_truth/ | sort -n | xargs -I {} sh -c "cat $OUTPUTDIR/beam_ground_truth/{}" >> $OUTPUTDIR/targets.txt
+ls $OUTPUTDIR/$MODELNAME/beam_ground_truth/ | sort -n | xargs -I {} sh -c "cat $OUTPUTDIR/$MODELNAME/beam_ground_truth/{}" >> $OUTPUTDIR/$MODELNAME/targets.txt
 
 cd ~/VertMetric/
 python vert.py score \
-	 --generated=$OUTPUTDIR/generated.txt \
-	  --target=$OUTPUTDIR/targets.txt \
-	   --out_dir=$OUTPUTDIR \
+	 --generated=$OUTPUTDIR/$MODELNAME/generated.txt \
+	  --target=$OUTPUTDIR/$MODELNAME/targets.txt \
+	   --out_dir=$OUTPUTDIR/$MODELNAME \
 	    --rouge_type f-measure
